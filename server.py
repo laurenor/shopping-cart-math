e9295c838f28c06bbbcbdf643a499c03125553ba
Lauren <laurenortencio@gmail.com>
2015-10-24 14:30:10 -0700

    added server

---
+++ server.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template, redirect, request
+from jinja2 import StrictUndefined
+
+app = Flask(__name__)
+
+app.secret_key = 'ABC'
+app.jinja_env.undefined = StrictUndefined
+
+@app.route('/')
+def index():
+	"""Return index page"""
+	return render_template('index.html')
+
+if __name__ == "__main__":
+    app.debug = True
+
+    connect_to_db(app)
+
+    app.run()
