57b648c5987528f71bec4a5c18517f1f1f20770c
Susan <susancodes@gmail.com>
2015-10-24 17:01:05 -0700

    adding ajax call for items

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect, request
+from flask import Flask, render_template, redirect, request, jsonify
 from jinja2 import StrictUndefined
 
 import requests
--- static/items.js
+++ static/items.js
@@ -2,14 +2,18 @@ function getProductDescription() {
 
   var url = "/shopping_item.json"
 
-  $.get(url, function (data){
+  $.get(url, function (results){
+
+  		var data = JSON.parse(results);
 
 		console.log(data);
 
-		// $("#song-title").text(data.song_name);
+		$("#product-price").text(data.song_name);
 		// $("#song-artist").text(" by " + data.artist + " [" + data.genre + "]");     
-		// $("#song-spotify").html("<a href='" + data.spotify + "'>Play on Spotify</a>")     
+		// $("#song-spotify").html("<a href='" + data.spotify + "'>Play on Spotify</a>")  ;   
 
 
   })
 }
+
+$("#get-item").on("click", getProductDescription)
